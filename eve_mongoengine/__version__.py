291694c7ca5dce66d13d5dd8a6636789070d77b0
Jaroslav Kulheim <kulheim@wdt.cz>
2020-09-28 15:09:18 +0200

    Version 0.1.1

--- eve_mongoengine/__version__.py
+++ eve_mongoengine/__version__.py
@@ -3,7 +3,7 @@
 # This file must remain compatible with
 # both Python >= 2.6 and Python 3.3+
 
-VERSION = (0, 1, 0)     # 0.1.0
+VERSION = (0, 1, 1)     # 0.1.1
 
 def get_version():
     if isinstance(VERSION[-1], int):
