96daedaa5720c1ae9d13e8b2bf1fa0aa2aecd25c
Jaroslav Kulheim <kulheim@wdt.cz>
2021-11-29 12:02:14 +0100

    Don't set updated for new documents MPI-000

--- eve_mongoengine/__init__.py
+++ eve_mongoengine/__init__.py
@@ -247,8 +247,11 @@ def fix_last_updated(sender, document, **kwargs):
     """
     Hook which updates LAST_UPDATED field before every Document.save() call.
     """
+    # is_new is magnetpro proprietary parameter used to indicate new document
+    if not kwargs.get("is_new", False):
     from eve.utils import config
-    field_name = config.LAST_UPDATED.lstrip('_')
+
+        field_name = config.LAST_UPDATED.lstrip("_")
     if field_name in document:
         document[field_name] = get_utc_time()
 
--- eve_mongoengine/__version__.py
+++ eve_mongoengine/__version__.py
@@ -3,7 +3,7 @@
 # This file must remain compatible with
 # both Python >= 2.6 and Python 3.3+
 
-VERSION = (0, 1, 1)     # 0.1.1
+VERSION = (0, 1, 2)     # 0.1.2
 
 def get_version():
     if isinstance(VERSION[-1], int):
